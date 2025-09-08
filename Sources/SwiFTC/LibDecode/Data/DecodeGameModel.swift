728650a84cd690dc8360e8b3e42e637103d1ce64
Jining Liu <hello@jining.dev>
2025-09-08 00:10:11 -0500

    0.9.7: added game labels

--- Sources/SwiFTC/LibDecode/Data/DecodeGameModel.swift
+++ Sources/SwiFTC/LibDecode/Data/DecodeGameModel.swift
@@ -15,6 +15,17 @@ public final class DecodeGameModel {
     @Attribute(.externalStorage) private var _teams: GameTeamsV1?
     @Attribute(.externalStorage) private var _scores: DecodeGameScores?
     public var timestamp: Date
+    
+    public init(
+        scores: DecodeGameScores,
+        teams: GameTeamsV1 = .init(),
+        timestamp: Date = Date()
+    ) {
+        self.id = UUID().uuidString
+        self._teams = teams
+        self._scores = scores
+        self.timestamp = timestamp
+    }
 
     public var teams: GameTeamsV1 {
         get { _teams ?? .init() }
@@ -26,14 +37,41 @@ public final class DecodeGameModel {
         set { _scores = newValue }
     }
 
-    public init(
-        scores: DecodeGameScores,
-        teams: GameTeamsV1 = .init(),
-        timestamp: Date = Date()
-    ) {
-        self.id = UUID().uuidString
-        self._teams = teams
-        self._scores = scores
-        self.timestamp = timestamp
+    public var label: GameLabel {
+        let red1 = self.teams.red.one
+        let red2 = self.teams.red.two
+
+        let redLabel: String
+        if let r1 = red1, let r2 = red2 {
+            redLabel = "\(r1) & \(r2)"
+        } else if let r1 = red1 {
+            redLabel = "\(r1) & Team"
+        } else if let r2 = red2 {
+            redLabel = "Team & \(r2)"
+        } else {
+            redLabel = "Red Alliance"
+        }
+
+        let blue1 = self.teams.blue.one
+        let blue2 = self.teams.blue.two
+
+        let blueLabel: String
+        if let b1 = blue1, let b2 = blue2 {
+            blueLabel = "\(b1) & \(b2)"
+        } else if let b1 = blue1 {
+            blueLabel = "\(b1) & Team"
+        } else if let b2 = blue2 {
+            blueLabel = "Team & \(b2)"
+        } else {
+            blueLabel = "Blue Alliance"
+        }
+
+        if red1 == nil && red2 == nil && blue1 == nil
+            && blue2 == nil
+        {
+            return .init(red: "", middle: "Game", blue: "")
+        }
+
+        return .init(red: redLabel, middle: " v.s. ", blue: blueLabel)
     }
 }
---
+++ Sources/SwiFTC/Shared/GameLabel.swift
@@ -0,0 +1,14 @@
+//
+//  GameLabel.swift
+//  SwiFTC
+//
+//  Created by Jining Liu on 9/8/25.
+//
+
+import Foundation
+
+public struct GameLabel {
+    public var red: String
+    public var middle: String
+    public var blue: String
+}
